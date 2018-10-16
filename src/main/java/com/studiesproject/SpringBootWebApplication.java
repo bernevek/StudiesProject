ca7d0543abad1c3ef2b182b9e2ca830356b90de6
Larionovv <larionovv@i.ua>
2018-10-16 19:24:54 +0300

    Added "bla bla bla"

--- src/main/java/com/studiesproject/SpringBootWebApplication.java
+++ src/main/java/com/studiesproject/SpringBootWebApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBootWebApplication {
 
     public static void main(String[] args) {
+        System.out.println("bla bla bla");
         SpringApplication.run(SpringBootWebApplication.class, args);
     }
 
